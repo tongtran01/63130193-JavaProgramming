7f8791a832d5b86feaef9e6e599137cb6b036155
tongtran01 <145564950+tongtran01@users.noreply.github.com>
2024-01-15 00:34:55 +0700

    Cập nhật code
    Sưả mới và thêm dòng

--- HelloWorld/src/till/edu/XinChao.java
+++ HelloWorld/src/till/edu/XinChao.java
@@ -14,6 +14,7 @@ public class XinChao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Lần đầu học Java và đang thử nghiệm quản lý mã nguồn với GIT");
+		System.out.print("Tôi thêm một dòng mới và sửa lại chữ Java ở trên");
 
 	}
 
