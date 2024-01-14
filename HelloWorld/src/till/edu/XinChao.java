4d10882f880ef264e2946a716fbfc46450bb6512
tongtran01 <145564950+tongtran01@users.noreply.github.com>
2024-01-15 00:32:01 +0700

    Ví dụ đầu tiên
    Chỉ để demo

---
+++ HelloWorld/src/till/edu/XinChao.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package till.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Lần đầu học Java và đang thử nghiệm quản lý mã nguồn với GIT");
+
+	}
+
+}
